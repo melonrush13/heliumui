3a5c34c641b0c07db345eaef2197dba6c81388f7
Melanie Rush <melanierush@erikmuns-8570w.northamerica.corp.microsoft.com>
2019-05-01 15:58:06 -0600

    adding rest api template

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,49 @@
-import React from 'react';
+import React from "react";
 import logo from './logo.svg';
 import './App.css';
-
+import axios from "axios";
+ 
 const App: React.FC = () => {
+
+  const state = {
+    genres: []
+  };
+  
+  function componentDidMount() {
+    axios
+      .get("https://heliumint.azurewebsites.net")
+      .then(response => {
+        // create an array of data
+        const genres = response.data.map((c:any) => {
+          return {
+            id: c.id,
+            genre: c.genre
+          }
+        });
+        const newState = Object.assign({}, this.state, {
+          genres: genres
+        });
+
+        // store the new state object in the component's state
+        this.setState(newState);
+        
+      })
+    .catch(error => console.log(error));
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Hi! 
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <table>
+          <tbody>
+            <tr>
+              <td> {this.state.genres} </td>
+            </tr>
+          </tbody>
+        </table>
       </header>
     </div>
   );
