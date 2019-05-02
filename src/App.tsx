14ef3ed505e8ea13a6df0b15f926157812d0a20b
Melanie Rush <melanierush@erikmuns-8570w.northamerica.corp.microsoft.com>
2019-05-02 15:32:54 -0600

    testing displaying all data

--- src/App.tsx
+++ src/App.tsx
@@ -9,36 +9,67 @@ class App extends React.Component {
 
   state = {
     items: [],
-    isLoaded: false,
+    isLoading: true,
   };
   
   // invoked immediately after component is mounted 
   // good place to load data from a remote endpoint
   componentDidMount() {
     console.log("Start of Mount");
+    console.log(this.state.isLoading);
 
-    axios
+
+    //function getGenres() {
+      axios
       .get('https://cors-anywhere.herokuapp.com/' + heliumApi)
       .then(response => {
         console.log('arrived');
-        console.log(response.data);
+
+        this.setState({
+          isLoading:false,
+          items: response.data[1].id,
+        })
+        console.log(response.data[1].id);
+        console.log(this.state.isLoading);
+
+
+
+        const newContacts = response.data.map((c:any) => {
+          console.log('hi ' + response.data[c].id);
+
+          return {
+            id: c.id,
+            name: c.name
+          };
+        });
+
+
+
+
+
       })
+
+      
+
       .catch(error => {
         console.log(error);
       });
+   // }
+   
   }
 
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <p>
-            Hi! 
-          </p>
+          <ul>genres: {this.state.items}</ul>
+          <p>State: {this.state.isLoading}</p>
           <table>
             <tbody>
-              <tr>
-              </tr>
+              {this.state.items.map((item) => {return <tr>
+                <td>item.id</td>
+                <td>item.genre</td>
+              </tr>})}
             </tbody>
           </table>
         </header>
