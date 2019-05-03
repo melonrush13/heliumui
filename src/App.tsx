a2c81ef16a4db76dc8a1f4b052532c66f57ce83f
Melanie Rush <melanierush@erikmuns-8570w.northamerica.corp.microsoft.com>
2019-05-03 12:55:12 -0600

    working map functionality with dog array woooohoooo '

--- src/App.tsx
+++ src/App.tsx
@@ -6,51 +6,36 @@ import axios from "axios";
 const heliumApi = 'https://heliumint.azurewebsites.net/api/';
 const cors = 'https://cors-anywhere.herokuapp.com/';
 
+const dogs = [
+  {id: 1, breed: 'beagle', name: 'snoopy'},
+  {id: 2, breed: 'dalmation', name: 'QT'},
+  {id: 3, breed: 'palmaranian', name: 'puppy'},
+];
+
 class App extends React.Component {
 
   state = {
-    genreItems: [],
-    isLoading: true,
-    genre: null,
-    data: null,
+    names: null,
   };
   
   // invoked immediately after component is mounted 
   // good place to load data from a remote endpoint
-  componentDidMount() {
-    console.log("Start of Mount");
-  
-      axios
-      .get(cors + heliumApi + 'genres')
-      .then(response => {
+  componentDidMount() {  
+    const names = dogs.map(p => p.name);
+    console.log(names);
 
-        this.setState({
-          isLoading:false,
-          genre: response.data[0].id,
-          data: null,
-        })
-
-        const data = response.data.genre;
-        this.setState({data});
-
-        // const newGenres = response.data.map((c:any) => {
-        //   console.log('hi ' + response.data[c].id);
-        //   return {
-        //     id: c.id,
-        //     name: c.name,
-        //   };
-        // });
-
-        // const newState = Object.assign({}, this.state, {
-        //   genreItems: newGenres
-        // });
-
-        // this.setState(newState)
-
-      })
-      .catch(error => {
-        console.log(error);
-      });   
+      // axios
+      // .get(cors + heliumApi + 'genres')
+      // .then(response => {
+      //   this.setState({
+      //     isLoading:false,
+      //     genre: response.data[0].id,
+      //     data: null,
+      //   })
+      // })
+      // .catch(error => {
+      //   console.log(error);
+      // });   
   }
 
   render() {
@@ -59,14 +44,11 @@ class App extends React.Component {
         <header className="App-header">
         <h1>Welcome to the Helium UI Application</h1>
         <h4>View data</h4>
-          <p>genres: {this.state.genre}</p>
-          <p>genres: {this.state.data}</p>
+          <ul>
+            {dogs.map(p => <p key={p.id}>{p.name}</p>)}
+          </ul>
           <table>
             <tbody>
-              {this.state.genreItems.map((genre) => {return <tr>
-                <td>genre.id</td>
-                <td>genre.genre</td>
-              </tr>})}
             </tbody>
           </table>
         </header>
