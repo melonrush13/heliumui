64009ca9f7f1bb862fbc70a4bf19e43d818c66a1
Melanie Rush <merush@microsoft.com>
2019-05-08 14:37:03 -0600

    adding movies and actors endpoints

--- src/App.tsx
+++ src/App.tsx
@@ -9,35 +9,89 @@ class App extends React.Component {
 
   state = {
     genres: [{id: null, name: null}],
+    movies: [{id: null, title: null}],
+    actors: [{id: null, name: null}],
+    genresDisplay: true,
+    moviesDisplay: true,
+    actorsDisplay: true,
+
   };
+
+
+  onGenresClick = (event: any) => {
+    console.log("genres display!");
+    this.setState({genresDisplay: !this.state.genresDisplay})
+  }
+
+  onMoviesClick = (event: any) => {
+    console.log("movies display!");
+    this.setState({moviesDisplay: !this.state.moviesDisplay})
+  }
+
+  onActorsClick = (event: any) => {
+    console.log("actors display!");
+    this.setState({actorsDisplay: !this.state.actorsDisplay})
+  }
   
   //testing github
   componentDidMount() {  
-      axios
-      .get(cors + heliumApi + 'genres')
-      .then(response => {
-        const genredata = response.data.map((item: any) => ({
+      axios.get(cors + heliumApi + 'genres').then(response => {
+        const genreData = response.data.map((item: any) => ({
           id: item.id,
           name: item.genre
         }))
         this.setState({
-          genres: genredata
+          genres: genreData
         })
       })
+      axios.get(cors + heliumApi + 'movies').then(response => {
+        const moviesData = response.data.map((item: any) => ({
+          id: item.id,
+          title: item.title
+        }))
+        this.setState({
+          movies: moviesData
+        })
+      })
+      axios.get(cors + heliumApi + 'actors').then(response => {
+        const actorsData = response.data.map((item: any) => ({
+          id: item.id,
+          name: item.name
+        }))
+        this.setState({
+          actors: actorsData
+        })
+      })
+
       .catch(error => {
         console.log(error);
-      });   
+      });
+
   }
 
   render() {
     return (
       <div className="App">
         <h1>Welcome to the Helium UI Application</h1>
-        <h2>Genres API</h2>
         <ul>
-        {this.state.genres.map(item => (
+          <button onClick={this.onMoviesClick}>Movies</button>
+          <button onClick={this.onGenresClick}>Genres</button>
+          <button onClick={this.onActorsClick}>Actors</button>
+        </ul>
+        <ul>
+        {this.state.genresDisplay && this.state.genres.map(item => (
               <ul>{item.id}</ul>
             ))}
+        </ul>
+        <ul>
+        {this.state.moviesDisplay && this.state.movies.map(item => (
+              <ul>{item.title}</ul>
+          ))}
+        </ul>
+        <ul>
+        {this.state.actorsDisplay && this.state.actors.map(item => (
+              <ul>{item.name}</ul>
+          ))}
         </ul>
       </div>
     );
